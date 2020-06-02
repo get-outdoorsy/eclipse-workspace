3d57ec5cb7e174c1e0aa2f7336a3c737970cfb66
patotie <34236837+patotie@users.noreply.github.com>
2020-06-02 21:00:32 +0800

    nakagawa ako ng date time
    oki

---
+++ javaEclipseTest/src/javaEclipseTest/EnumNaNga.java
@@ -0,0 +1,14 @@
+package javaEclipseTest;
+
+public enum EnumNaNga {
+	LOW,
+	MEDIUM,
+	HIGH,
+	LODI
+	
+	
+
+}
+//enums ay nag sstore lang constant values, di to mababago
+// di makagawa ng objects, di rin maextend pero pwede maka implement ng interfaces
+//use enums sa non changeable values gaya ng months days etc
